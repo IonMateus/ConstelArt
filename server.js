c4ca22fc7c0901d647ae3c899c66da0014dc6ebd
zBl4ckUser <zBl4ckDev@protonmail.com>
2024-10-05 15:30:41 -0300

    gadjgs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const PORT = 8080
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'))
 })
