e9e724beb18522dc0a48dfa403d64ae13d6e6756
arthurgamajorge <arthurgamajorgetec@gmail.com>
2024-10-06 20:09:25 -0300

    em honra a pedro...

--- public/scripts/Mapa.js
+++ public/scripts/Mapa.js
@@ -389,6 +389,21 @@ carregarEstrelas().then(() => {
         }
     
         zoomSpeed *= 0.9; // Reduzir a velocidade do zoom ao longo do tempo
+
+        const cameraDirection = new THREE.Vector3();
+        camera.getWorldDirection(cameraDirection);
+        cameraDirection.y = 0; // Mantenha o eixo Y constante
+        cameraDirection.normalize();
+    
+        const cameraRight = new THREE.Vector3();
+    camera.getWorldDirection(cameraRight);
+    cameraRight.cross(camera.up); // Direção à direita
+    cameraRight.normalize();
+
+    if (moveForward) camera.position.add(cameraDirection.multiplyScalar(moveSpeed));
+    if (moveBackward) camera.position.add(cameraDirection.multiplyScalar(-moveSpeed));
+    if (moveLeft) camera.position.add(cameraRight.multiplyScalar(-moveSpeed));
+    if (moveRight) camera.position.add(cameraRight.multiplyScalar(moveSpeed));
     
         raycaster.setFromCamera(mouse, camera);
         const intersects = raycaster.intersectObjects(estrelas.map(estrela => estrela.mesh));
@@ -535,3 +550,50 @@ carregarEstrelas().then(() => {
     
 });
 
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+const moveSpeed = 0.1; // Velocidade de movimento da câmera
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'w':
+        case 'W':
+            moveForward = true;
+            break;
+        case 's':
+        case 'S':
+            moveBackward = true;
+            break;
+        case 'a':
+        case 'A':
+            moveLeft = true;
+            break;
+        case 'd':
+        case 'D':
+            moveRight = true;
+            break;
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'w':
+        case 'W':
+            moveForward = false;
+            break;
+        case 's':
+        case 'S':
+            moveBackward = false;
+            break;
+        case 'a':
+        case 'A':
+            moveLeft = false;
+            break;
+        case 'd':
+        case 'D':
+            moveRight = false;
+            break;
+    }
+});
